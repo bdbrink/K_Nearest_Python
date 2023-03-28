8cc11f38392a041a400daf557ec19a6bb08e9071
bdbrink <brendan.brink@outlook.com>
2023-03-28 17:35:43 -0500

    def euclidean distnce

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 
+point = {"blue": [[2,4], [1,3], [2,3], [3,2], [2,1]],
+        "red": [[5,6], [4,5], [4,6], [6,6], [5,4]]}
+
+new_point = [3,3]
+
+def euclidean_distance(p, q):
+    np.sqrt(np.sum((np.array(p) - np.array(q)) ** 2))
